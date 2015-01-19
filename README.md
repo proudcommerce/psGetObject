11b03b8774c79c63d48d0ee04627b916b8b813b7
Tobias Merkl <merkl@proudsourcing.de>
2015-01-19 09:29:44 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# psGetObject
+psGetObject
+=========
+
 Smarty plugin to load any kind of an oxid object in a template.
+Free Extension for OXID eShop.
+	 
+
+Usage
+
+	[{ psgetobject type="oxarticle" ident="abc123" field="oxtitle" assign="oProduct" lang=1 }]
+
+
+Parameters
+
+	type	/	oxid type
+	ident	/	identifier e. g. oxid
+	assign	/	template variable
+	field	/	returns only a specified field from a object (optional)
+	lang	/	language id (optional)
+	
+
+Changelog
+
+	1.0.0 - Release
+	
+	
+License
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+
+Copyright
+
+	Proud Sourcing GmbH 2015
+	www.proudcommerce.com
---
+++ copy_this/smarty/plugins/function.psgetobject.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @copyright (c) Proud Sourcing GmbH | 2015
+ * @link www.proudcommerce.com
+ * @package psGetObject
+ * @version 1.0.0
+ **/
+ 
+/**
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File: function.getobject.php
+ * Type: function
+ * Name: getPsObject
+ * Purpose: Output core class object
+ * add [{ psgetobject type="oxarticle" ident="abc123" field="oxtitle" assign="oProduct" lang=1 }] where you want to display content
+ * -------------------------------------------------------------
+ *
+ * @param $params
+ * @param $smarty Smarty
+ *
+ * @return oxbase
+ * @throws Exception
+ */
+function smarty_function_psgetobject( $params, &$smarty )
+{
+    $iLang = oxRegistry::getLang()->getBaseLanguage();
+    $sIdent = isset( $params['ident'] ) ? (string)$params['ident'] : '';
+    $sType = isset( $params['type'] ) ? (string)$params['type'] : '';
+    $sField = isset( $params['field'] ) ? (string)$params['field'] : '';
+    $sAssign = isset( $params['assign'] ) ? (string)$params['assign'] : '';
+    $iLang = isset( $params['lang'] ) ? (int)$params['lang'] : $iLang;
+    $mRet = null;
+
+    if($sType == "")
+    {
+        throw new Exception('You need to define an object type! Use type="myClass".');
+    }
+
+    $oObject = oxNew($sType);
+    if($sIdent != "" && $oObject->loadInLang($iLang, $sIdent) == false)
+    {
+        throw new Exception("Couldn't load ident: $sIdent");
+    }
+
+    if($sField)
+    {
+        $mRet = $oObject->getFieldData($sField);
+    }
+    else
+    {
+        $mRet = $oObject;
+    }
+
+    if($sAssign)
+    {
+        $smarty->assign($sAssign, $mRet);
+    }
+    else
+    {
+        return $mRet;
+    }
+
+    return null;
+}
